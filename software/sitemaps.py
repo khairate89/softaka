8d8f62fe84a8ca8d14cb22a115ff53caedce435f
khairate89 <khairateabdessamad@gmail.com>
2025-07-31 16:19:37 +0200

    Update sitemaps.py

--- software/sitemaps.py
+++ software/sitemaps.py
@@ -1,52 +1,29 @@
-# software/sitemaps.py
-
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 from django.conf import settings
-from .models import Software
-
-class StaticViewSitemap(Sitemap):
-    priority = 0.5
-    changefreq = 'monthly'
-
-    def items(self):
-        # Use namespaced view names
-        return ['software:dmca', 'software:disclaimer', 'software:privacy_policy',
-                'software:terms_conditions', 'software:sitemap_page',
-                'software:contact_us', 'software:about_us']
-
-    def location(self, item):
-        return reverse(item)
-
-    def alternates(self, item):
-        alternates = []
-        for lang_code, _ in settings.LANGUAGES:
-            alternates.append({
-                'lang': lang_code,
-                'url': f'/{lang_code}{self.location(item)}'
-            })
-        return alternates
-
+from django.utils.translation import activate, get_language
 
 class SoftwareSitemap(Sitemap):
-    priority = 0.9
-    changefreq = 'weekly'
+    changefreq = "weekly"
+    priority = 0.8
 
     def items(self):
         return Software.objects.all()
 
     def location(self, obj):
-        # Default language URL
-        lang_code = settings.LANGUAGE_CODE
-        category_slug = getattr(obj.category, f'slug_{lang_code}', obj.category.slug)
-        software_slug = getattr(obj, f'slug_{lang_code}', obj.slug)
-        return f'/{lang_code}/{category_slug}/{software_slug}/'
+        return obj.get_absolute_url()
 
     def alternates(self, obj):
         alternates = []
+        current_lang = get_language()
         for lang_code, _ in settings.LANGUAGES:
-            category_slug = getattr(obj.category, f'slug_{lang_code}', obj.category.slug)
-            software_slug = getattr(obj, f'slug_{lang_code}', obj.slug)
-            url = f'/{lang_code}/{category_slug}/{software_slug}/'
-            alternates.append({'lang': lang_code, 'url': url})
+            if lang_code == current_lang:
+                continue
+            activate(lang_code)
+            url = obj.get_absolute_url()
+            alternates.append({
+                'lang_code': lang_code,
+                'url': url,
+            })
+        activate(current_lang)
         return alternates
