2227d5b4ba62518054cca54110f579c1aee3fbff
khairate89 <khairateabdessamad@gmail.com>
2025-07-31 16:36:30 +0200

    Update urls.py

--- filecr_clone/urls.py
+++ filecr_clone/urls.py
@@ -1,29 +1,27 @@
+# filecr_clone/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 from django.http import HttpResponseRedirect
 from django.conf import settings
 from software import views as software_views
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.sitemaps.views import sitemap
 from software.sitemaps import SoftwareSitemap, StaticViewSitemap, CategorySitemap
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 def redirect_to_language_prefix(request):
-    """
-    Redirects the root URL ('/') to the default language prefix (e.g., '/en/').
-    """
     return HttpResponseRedirect(f'/{settings.LANGUAGE_CODE}/')
 
 
 sitemaps = {
-    'static': StaticViewSitemap,
-    'software': SoftwareSitemap,
-    'categories': CategorySitemap,
+    'static': StaticViewSitemap(),
+    'software': SoftwareSitemap(),
+    'categories': CategorySitemap(),
 }
 
-# Base urlpatterns for non-translated URLs
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('nested_admin/', include('nested_admin.urls')),
@@ -34,12 +32,10 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
 
-# Translated URLs with language prefix (e.g. /en/, /fr/)
 urlpatterns += i18n_patterns(
     path('', include('software.urls')),
 )
 
-# Serve static and media files in development only
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
