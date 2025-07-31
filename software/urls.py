8df50088a2680766c9a5c4bb12d6781b29c8a865
khairate89 <khairateabdessamad@gmail.com>
2025-07-31 18:01:07 +0200

    Update urls.py

--- software/urls.py
+++ software/urls.py
@@ -16,7 +16,8 @@ urlpatterns = [
 
     path('file-download/', views.FileDownloadView.as_view(), name='file-download'),
     path('rate-software/', views.rate_software, name='rate_software'),
-      path('dmca/', TemplateView.as_view(template_name='software/dmca.html'), name='dmca'),
+    path('', TemplateView.as_view(template_name='software/index.html'), name='home'),
+    path('dmca/', TemplateView.as_view(template_name='software/dmca.html'), name='dmca'),
     path('disclaimer/', TemplateView.as_view(template_name='software/disclaimer.html'), name='disclaimer'),
     path('privacy-policy/', TemplateView.as_view(template_name='software/privacy_policy.html'), name='privacy_policy'),
     path('terms-conditions/', TemplateView.as_view(template_name='software/terms_conditions.html'), name='terms_conditions'),
